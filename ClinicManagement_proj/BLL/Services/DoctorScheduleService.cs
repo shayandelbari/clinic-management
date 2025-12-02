fd22b1ab77938040e059901f802e53ee5aafb3da
shayan <shayan.delbari@protonmail.com>
2025-12-01 21:31:44 -0500

    Finished DoctorSchduleService;

--- ClinicManagement_proj/BLL/Services/DoctorScheduleService.cs
+++ ClinicManagement_proj/BLL/Services/DoctorScheduleService.cs
@@ -2,6 +2,7 @@
 using ClinicManagement_proj.BLL.Services;
 using ClinicManagement_proj.DAL;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ClinicManagement_proj.BLL
@@ -57,5 +58,53 @@ namespace ClinicManagement_proj.BLL
             clinicDb.SaveChanges();
         }
 
+        public List<DoctorScheduleDTO> GetAllSchedules()
+        {
+            if (!ClinicManagementApp.CurrentUserHasRole
+                (
+                    UserService.UserRoles.Administrator,
+                    UserService.UserRoles.Doctor,
+                    UserService.UserRoles.Receptionist
+                )
+            )
+                throw new UnauthorizedAccessException("You don't have access to read all schedules");
+            return clinicDb.DoctorSchedules.ToList();
+        }
+
+        public DoctorScheduleDTO Search(int id)
+        {
+            if (!ClinicManagementApp.CurrentUserHasRole
+                (
+                    UserService.UserRoles.Administrator,
+                    UserService.UserRoles.Doctor,
+                    UserService.UserRoles.Receptionist
+                )
+            )
+                throw new UnauthorizedAccessException("You don't have access to search schedules");
+
+            var schedule = clinicDb.DoctorSchedules
+                .Where(s => s.Id == id)
+                .FirstOrDefault()
+                ?? throw new ArgumentException("Schedule not found");
+            return schedule;
+        }
+
+        public DoctorScheduleDTO Search(string dayOfWeek)
+        {
+            if (!ClinicManagementApp.CurrentUserHasRole
+                (
+                    UserService.UserRoles.Administrator,
+                    UserService.UserRoles.Doctor,
+                    UserService.UserRoles.Receptionist
+                )
+            )
+                throw new UnauthorizedAccessException("You don't have access to search schedules");
+
+            var schedule = clinicDb.DoctorSchedules
+                .Where(s => s.DayOfWeek == dayOfWeek)
+                .FirstOrDefault()
+                ?? throw new ArgumentException("Schedule not found");
+            return schedule;
+        }
     }
 }
